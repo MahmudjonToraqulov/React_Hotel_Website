2adf7924d1fa0e7ea0952366f09d24a4156acffc
Makhmoodjan <meexample@gmail.com>
2022-04-30 10:30:19 +0300

    First Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./common/Navbar/Navbar";
+import './App.css'
+import {BrowserRouter as Router } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">    
+      <Router>
+       <Navbar />
+       <Switch>
+         <Route exact path='/' />
+       </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/common/Navbar/Navbar.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css' 
+
+
+const Navbar = () => {
+    const [ click,setClick ] = useState(false)
+
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
+
+  return (
+      <>
+        <nav className='navbar'>
+            <div className='container flex_space'>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className = { click ? ' fas fa-times ' : ' fas fa-bars ' }></i>
+                </div>
+
+                <ul className={ click ? 'nav-menu active ' : "nav-menu" }> 
+                        <li><Link to='/' onClick={closeMobileMenu}> Home </Link></li>
+                        <li><Link to='/about' onClick={closeMobileMenu}> About </Link></li>
+                        <li><Link to='/gallery' onClick={closeMobileMenu}> Gallery</Link></li>
+                        <li><Link to='/destination' onClick={closeMobileMenu}> Destination </Link></li>
+                        <li><Link to='/blog' onClick={closeMobileMenu}> Blog </Link></li>
+                        <li><Link to='/testimonials' onClick={closeMobileMenu}> Testimnials </Link></li>
+                        <li><Link to='/contact' onClick={closeMobileMenu}> Contact Us </Link> </li>
+                </ul>
+
+                <div className='login-area flex'> 
+                        <li>
+                            <Link to='sign-in'>
+                                <i className='fas fa-chevron-right'>Sign in</i>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='register'>
+                                <i className=' fas fa-chevron-right '>Register</i>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='contact'>
+                                <button className='primary-btn'> Request a Quote </button>
+                            </Link>
+                        </li> 
+                </div>
+            </div>
+        </nav>
+
+        <header >
+            <div className='container flex_space'>
+                <div className='logo'>
+                    <img src="../images/logo.png" />
+                </div>
+            </div>
+        </header>
+      </>
+ 
+  )
+}
+
+export default Navbar
